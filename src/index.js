4465501caa9a3e58b1392b533f04d44758177124
keldenl <keldenlin@gmail.com>
2022-03-30 21:20:45 -0700

    migrate to hashrouter so gh-pages supports it

--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
 import React from 'react';
 import ReactGA from 'react-ga';
 import { render } from 'react-dom';
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 
 import Main from './Main';
 import './styles/index.css';
 
 
-ReactGA.initialize( 'UA-57882595-2');
+ReactGA.initialize('UA-57882595-2');
 ReactGA.pageview(window.location.pathname + window.location.search);
 
 const rootElement = document.getElementById("root");
 const app = (
-  <BrowserRouter>
+  <HashRouter>
     <Main />
-  </BrowserRouter>
+  </HashRouter>
 )
 // if (rootElement.hasChildNodes()) {
 // hydrate(app, rootElement);
